2d16c006a6c831db074f6188365ef37867b9ef4d
Ozobia Nwadibia <Ozobia.cn3@gmail.com>
2020-05-05 07:59:26 -0500

    Worked on the scanner object

---
+++ Problems/Print strings in the reverse order/src/Main.java
@@ -0,0 +1,22 @@
+//put imports you need here
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter first word: ");
+        String input1 = sc.next();
+
+//        System.out.print("Enter second word: ");
+        String input2 = sc.next();
+
+//        System.out.print("Enter third word: ");
+        String input3 = sc.next();
+
+        System.out.println(input3);
+        System.out.println(input2);
+        System.out.println(input1);
+    }
+}
