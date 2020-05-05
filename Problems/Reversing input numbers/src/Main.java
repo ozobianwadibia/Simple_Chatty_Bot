d81acfcc300df2a6d5881fb805da1dc45be09c2b
Ozobia Nwadibia <Ozobia.cn3@gmail.com>
2020-05-05 18:15:47 -0500

    Finished decrement and reversing input numbers problem

---
+++ Problems/Reversing input numbers/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+//        System.out.println("Enter number(s): ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println(y + " " + x);
+
+    }
+}
--- Problems/Snail/src/Main.java
+++ Problems/Snail/src/Main.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-import static sun.jvm.hotspot.code.CompressedStream.H;
-
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
 //        System.out.println("Enter params here: ");
-        int height = scanner.nextInt();
-        int dayMvt = scanner.nextInt();
-        int nightMvt = scanner.nextInt();
+        int H = scanner.nextInt();
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
 
-        int mvCycle = dayMvt - nightMvt;
-        int height =
+        int R = A - B;
+        int rate = H / R;
+        System.out.println(rate);
+
 
     }
 }
