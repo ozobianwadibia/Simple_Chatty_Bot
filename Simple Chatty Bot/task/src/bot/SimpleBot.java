eccbe3f1662ebad0968b3c0497f4655cd5c258cf
Ozobia Nwadibia <Ozobia.cn3@gmail.com>
2020-05-07 08:57:41 -0500

    Continued work on the simple chatty bot, making it read name and age

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -11,14 +11,19 @@ public class SimpleBot {
         System.out.println("Please, remind me your name.");
 
         // reading a name
+        String name = scanner.nextLine();
 
-        System.out.println("What a great name you have, {yourName}!");
+        System.out.println("What a great name you have, " + name +"!");
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
         // reading all remainders
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
 
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
 
-        System.out.println("Your age is {yourAge}; that's a good time to start programming!");
+        System.out.println("Your age is " + age +";" + " that's a good time to start programming!");
     }
 }
