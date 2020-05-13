deb399942f07d3f70a022bc02d1a7f2773f3cec9
Ozobia Nwadibia <Ozobia.cn3@gmail.com>
2020-05-13 18:19:14 -0500

    Solved the 'arithmetic expression' problem

---
+++ Problems/The (un)-ordered sequence/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        int count = 0;
+        while ((number = scanner.nextInt()) != 0) {
+
+            System.out.println(number);
+        }
+    }
+}
